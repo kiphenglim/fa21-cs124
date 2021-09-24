2e5235bae8c93c78d23cd9811a431f9b27626581
Kip Lim <klim@hmc.edu>
2021-09-23 18:07:20 -0700

    create basic outline of list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+let id_counter = 2;
+let data = [
+  {
+    id: 0,
+    task: "Call Mom"
+  },
+  {
+    id: 1,
+    task: "Text John"
+  }
+]
+
+function handleAdd(newItem) {
+  console.log(newItem);
+  data.push({
+    id: id_counter,
+    text: newItem
+  });
+  id_counter += 1;
+}
+
+function List(props) {
+  const [completed, setCompleted] = useState([]);
+
+  return <form className="list" action={handleAdd("yes")}>
+    {props.listItems.map(item =>
+      <ListItem key={item.id} id={item.id} task={item.task}/>)}
+    <input type="text" id="new-item-text" name="new-item-create" />
+    <input type="submit" value="Add Item"/>
+  </form>
+}
+
+function ListItem(props) {
+  return <div className="item" id={props.id}>
+    <input type="checkbox" id={"item-" + props.id} name={"item-" + props.id}/>
+    <label htmlFor={"item-" + props.id}>{props.task}</label>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CS124 Lab 2</h1>
+      <List listItems={data} />
     </div>
   );
 }
