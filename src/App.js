375b30b34ac20adc6197d2d107708c8321055abf
Kip Lim <klim@hmc.edu>
2021-09-27 16:33:00 -0700

    add task to data array on 'add item' button click

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
 
-let id_counter = 2;
+let id_counter = 4;
 let data = [
   {
     id: 0,
@@ -22,48 +22,90 @@ let data = [
 ];
 
 function List(props) {
+  const [addingNewItem, setAddingNewItem] = useState("");
   const [isChecked, setIsChecked] = useState([]);
 
   function handleIsCheckedChange(e) {
     var newId = parseInt(e.target.id);
-    console.log(newId);
     if (isChecked.includes(newId)) {
-      setIsChecked(isChecked.filter(id => id !== newId));
+      setIsChecked(isChecked.filter((id) => id !== newId));
     } else {
       setIsChecked([...isChecked, newId]);
     }
   }
 
-  return <div>
-    {props.listItems.map(item =>
-      <ListItem
-        key={item.id}
-        id={item.id}
-        task={item.task}
-        onChange={handleIsCheckedChange}
-        checked={isChecked.includes(item.id)}/>)}
+  function handleAdd() {
+    data.push({
+      id: id_counter,
+      text: addingNewItem,
+    });
+    setAddingNewItem("");
+    id_counter += 1;
+    console.log(id_counter);
+    console.log(data);
+  }
 
-    <input type="text"
-      id="new-item-text"
-      name="new-item-create"
+  return (
+    <div>
+      <ListItemContainer
+        checked={isChecked}
+        listItems={props.listItems}
+        onChange={handleIsCheckedChange}
       />
-    <input type="button"
-      value="Add Item"
-    />
-  </div>
+      <AddItem
+        onClick={handleAdd}
+      />
+    </div>
+  );
+}
+
+function ListItemContainer(props) {
+  return (
+    <div>
+      {props.listItems.map((item) => (
+        <ListItem
+          checked={props.checked.includes(item.id)}
+          id={item.id}
+          key={item.id}
+          onChange={props.onChange}
+          task={item.task}
+        />
+      ))}
+    </div>
+  );
 }
 
 function ListItem(props) {
-  return <div className="item" id={props.id}>
-    <input type="checkbox"
-      id={props.id}
-      name={props.id}
-      onChange={props.onChange}/>
-    <label htmlFor={props.id}
-      className={props.checked ? "checked" : ""}>
-      {props.task}
-    </label>
-  </div>
+  return (
+    <div className="item" id={props.id}>
+      <input
+        id={props.id}
+        name={props.id}
+        onChange={props.onChange}
+        type="checkbox"
+      />
+      <label className={props.checked ? "checked" : ""} htmlFor={props.id}>
+        {props.task}
+      </label>
+    </div>
+  );
+}
+
+function AddItem(props) {
+  return (
+    <div>
+      <input
+        type="text"
+        id="new-item-text"
+        name="new-item-create"
+      />
+      <input
+        type="button"
+        value="Add Item"
+        onClick={props.onClick}
+      />
+    </div>
+  );
 }
 
 function App() {
