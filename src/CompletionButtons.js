a038336d1f0b5f4b1db247ca9145a85e9e68bf1f
Jennifer <jzli@hmc.edu>
2021-10-07 17:27:12 -0700

    css for completion buttons

--- src/CompletionButtons.js
+++ src/CompletionButtons.js
@@ -1,10 +1,10 @@
 function CompletionButtons(props) {
   return props.anyCompletedTasks ? (
-    <div>
-      <button onClick={props.onShowAllClick}>
+    <div className={"CompletionButtons"}>
+      <button className={"ShowUncompletedButton"} onClick={props.onShowAllClick}>
         {props.showingAllTasks ? "Show Uncompleted Tasks" : "Show All Tasks"}
       </button>
-      <button onClick={props.onRemoveAllClick}>
+      <button className={"RemoveCompletedButton"} onClick={props.onRemoveAllClick}>
         Remove Completed Tasks
       </button>
     </div>
