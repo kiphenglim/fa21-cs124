3e70647fa799191393a6a23fae1a1ab243944690
Jennifer <jzli@hmc.edu>
2021-10-11 15:16:48 -0700

    removed initial data, initial list should be empty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import "./App.css";
 import List from "./List";
 
-let initialData = [
-  {
-    id: 0,
-    task: "Call Mom",
-  },
-  {
-    id: 1,
-    task: "Text John",
-  },
-  {
-    id: 2,
-    task: "test task 1",
-  },
-  {
-    id: 3,
-    task: "test task 2",
-  },
-];
+let initialData = [];
 
 function App() {
   return (
