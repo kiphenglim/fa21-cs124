2d5cd775f192974e7d4a16d04bb39481e3bbd3df
Jennifer <jzli@hmc.edu>
2021-11-07 14:16:06 -0800

    change delete list icon to trashcan

--- src/ListMenuItem.js
+++ src/ListMenuItem.js
@@ -1,11 +1,19 @@
+import trashcan from './trashcan.png'
 
 function ListMenuItem(props) {
     return (
         <div>
             <div className={'ListMenuItem'}>
                 <div className={'list-menu-item-name'}>{props.listName}</div>
-                <button className={'list-menu-item-delete'}
-                        onClick={(e) => props.onDeleteList(props.id)}>Delete</button>
+                <button className={'ListMenuDelete'}
+                        onClick={(e) => props.onDeleteList(props.id)}>
+                    <img className={'DeleteIcon'}
+                         src={trashcan}
+                         alt='delete list'
+                         width={15}
+                         height={15}
+                    />
+                </button>
                 <button className={'list-menu-item-edit'}
                         onClick={(e) => props.onChangeDisplay(props.id)}>→</button>
 
