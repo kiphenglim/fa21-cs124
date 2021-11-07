a2184e6d58531b9a7b96c3dd6bd3b7412679da3b
Kip Lim <klim@hmc.edu>
2021-11-07 14:15:03 -0800

    wrap list buttons in div

--- src/ListMenuItem.js
+++ src/ListMenuItem.js
@@ -4,11 +4,12 @@ function ListMenuItem(props) {
         <div>
             <div className={'ListMenuItem'}>
                 <div className={'list-menu-item-name'}>{props.listName}</div>
-                <button className={'list-menu-item-delete'}
+                <div>
+                    <button className={'list-menu-item-delete'}
                         onClick={(e) => props.onDeleteList(props.id)}>Delete</button>
-                <button className={'list-menu-item-edit'}
-                        onClick={(e) => props.onChangeDisplay(props.id)}>→</button>
-
+                    <button className={'list-menu-item-edit'}
+                            onClick={(e) => props.onChangeDisplay(props.id)}>→</button>
+                </div>
             </div>
             <hr/>
         </div>
