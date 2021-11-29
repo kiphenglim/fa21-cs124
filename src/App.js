5b691d285dda89310e6568856d588dbc1d39d562
Kip Lim <klim@hmc.edu>
2021-11-29 14:53:25 -0800

    remove all files unrelated to authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import HeaderBar from './HeaderBar';
-import List from './List'
-import ListMenu from './ListMenu';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import TabList from './TabList';
 import loadingIcon from './loading.gif';
 
 import firebase from 'firebase/compat';
 import {GoogleAuthProvider} from 'firebase/auth';
-import {useState} from 'react';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollection} from 'react-firebase-hooks/firestore';
 
@@ -30,30 +26,8 @@ const gProvider = new GoogleAuthProvider();
 
 function App() {
   const [user, authLoading, authError] = useAuthState(auth);
-  /*
-  collection: collection of lists each with following properties:
-    id
-    created
-    name
-    sort
-    tasks
-    ownerId
-    sharedId
-   */
   const collection = db.collection('kiphenglim-lab5');
   const [value, dbLoading, dbError] = useCollection(collection);
-
-  const [currentDisplay, setCurrentDisplay] = useState('menu');
-
-  let lists = [];
-  if (value) {
-    lists = value.docs.map(e => { return e.data() });
-  }
-
-  function handleChangeDisplay(id) {
-    console.log(id);
-    setCurrentDisplay(id);
-  }
 
   if (authLoading) {
     return <p>Checking authentication</p>
@@ -69,32 +43,7 @@ function App() {
             userEmail={user.email}
             userId={user.uid}
           />
-          {currentDisplay === 'menu'
-            ?
-            // <TabList aria-label={'switch tabs to view owned or shared list'}>
-            //   <div key={'owned'}
-            //        aria-label={'list owned by me'}>
-            <ListMenu
-              collection={collection}
-              listItems={lists}
-              onChangeDisplay={handleChangeDisplay}
-              user={user.uid}
-            />
-            //   </div>
-            //   <div key={'shared'}
-            //        align={'center'}
-            //        aria-label={'lists shared with me'}>
-            //       Lists Shared With Me
-            //   </div>
-            // </TabList>
-            : <List
-              db={db}
-              id={currentDisplay}
-              listData={lists.find(e => e.id === currentDisplay)}
-              listDocRef={collection.doc(currentDisplay)}
-              listCollection={collection.doc(currentDisplay).collection('tasks')}
-              onChangeDisplay={handleChangeDisplay}
-            />}
+          <p>Yay! Auth worked :)</p>
         </div>
       }
   }
